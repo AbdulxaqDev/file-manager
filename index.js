501f8853af4fedfe096e3046d5fbe2657822d9c3
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-03 12:20:07 +0300

    feat: set-up stream flow

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+import { createInterface } from "node:readline";
 
+const cli = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+cli.setPrompt("Enter a command: ")
+cli.prompt(true)
+
+
+cli.on("line", (input) => {
+  console.log(`You enterd ${input}`);
+
+
+  if(input.includes(".exit")){
+    cli.close()
+  }else{
+    cli.prompt()
+  }
+
+})
+
+cli.on("close", () => {
+  console.log("Good Bye! :)")
+  process.exit(0);
+})
