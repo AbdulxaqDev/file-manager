f7a16604f373630fbdbb7c69dcd6c1016124ea36
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-05 22:32:49 +0300

    refactor: remove logs and update the code

--- src/helpers/dir.js
+++ src/helpers/dir.js
@@ -3,33 +3,15 @@ import { stat } from "node:fs/promises";
 
 import { __dirname } from "./dirname.js";
 
-let systemRootDir = __dirname(import.meta.url, "..", "..");
-let fManagerRootDir = "/";
+export let systemRootDir = __dirname(import.meta.url, "..", "..");
+export let fManagerRootDir = "/";
 
 export async function isExist(dir, flag) {
   try {
     const stats = await stat(join(systemRootDir, ...dir.split("/")));
-    if (flag === "dir") {
-      if (stats.isDirectory()) {
-        return true;
-      }
-      if (stats.isFile()) {
-        throw new Error(` the "${dir}" is NOT directory!\n`);
-      }
-      throw new Error(` the "${dir}" PATH does not exist!\n`);
-    }
-    if (flag === "file") {
-      if (stats.isFile()) {
-        return true;
-      }
-      if (stats.isDirectory()) {
-        throw new Error(` the "${dir}" is NOT a file!\n`);
-      }
-      throw new Error(` the "${dir}" FILE does not exist!\n`);
-    }
-  } catch (error) {
-    // if (error.code !== "ENOENT")
-    console.error(error);
+    return true;
+  } catch {
+    console.error("\nOperation failed");
     return false;
   }
 }
@@ -41,8 +23,6 @@ export function up() {
     join(...fManagerRootDir.split("/").slice(0, -1)) === "."
       ? "/"
       : join(...fManagerRootDir.split("/").slice(0, -1));
-  console.log("system root: ", systemRootDir);
-  console.log("fm root: ", fManagerRootDir);
 }
 
 export async function cd(dir) {
@@ -50,8 +30,6 @@ export async function cd(dir) {
     systemRootDir = join(systemRootDir, dir);
     fManagerRootDir =
       join(fManagerRootDir, dir) === "." ? "/" : join(fManagerRootDir, dir);
-    console.log("system root: ", systemRootDir);
-    console.log("fm root: ", fManagerRootDir);
   }
 }
 
