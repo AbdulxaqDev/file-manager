55d41e3cc2bf3cfa1d569e8de048d27649abe383
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2024-02-05 14:07:59 +0300

    fix: convert class to functions

--- src/helpers/dir.js
+++ src/helpers/dir.js
@@ -1,32 +1,61 @@
 import { join } from "node:path";
+import { stat } from "node:fs/promises";
 
 import { __dirname } from "./dirname.js";
 
-export default class Dir {
-  constructor() {
-    this.root = __dirname(import.meta.url, "..", "..");
-    this.wDir = ["/"];
-    this.curDir = this.wDir[this.wDir.length];
+let sysRootDir = __dirname(import.meta.url, "..", "..");
+let workingDir = ["/"];
+let curDir = workingDir[workingDir.length - 1];
+
+function getCurDir() {
+  return curDir;
+}
+
+function getWorkingDir() {
+  return join(...workingDir);
+}
+
+function getSysRootDir() {
+  return sysRootDir;
+}
+
+function up() {
+  if (curDir === "/") return;
+  // Real root directory
+  let newSysRoot = sysRootDir.split("/").slice(0, -1);
+  sysRootDir = join(...newSysRoot);
+
+  // Real root directory
+  workingDir.pop();
+  curDir = workingDir[workingDir.length - 1];
+
+  return join(...workingDir);
+}
+
+async function cd(dir) {
+  // Check the path existence
+  if (!(await isExist(dir.split("/")))) {
+    console.log(`\nthe "${dir}" path does not exist!\n`);
+    return;
   }
 
-  getCurDir() {
-    return this.curDir;
-  }
+  // Real root directory
+  sysRootDir = join(sysRootDir, dir);
 
-  up() {
-    if (this.wDir === "/") return;
+  // File manager directories
+  workingDir.push(...dir.split("/"));
+  curDir = workingDir[workingDir.length - 1];
 
-    this.wDir = this.wDir.pop();
-    this.curDir = this.wDir[this.wDir.length];
-  }
+  return join(...workingDir);
+}
 
-  cd(dir) {
-    this.wDir = this.wDir.push(dir);
-    this.curDir = this.wDir[this.wDir.length];
+async function isExist(dir) {
+  try {
+    const stats = await stat(join(sysRootDir, ...dir));
+    if (stats.isDirectory() || stats.isFile()) return true;
+  } catch {
+    return false;
   }
 }
 
-const test = new Dir();
-
-console.log(test.curDir);
-console.log(test.rootDir);
+export { getCurDir, getWorkingDir, getSysRootDir, up, cd, isExist };
